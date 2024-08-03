4139beb50f72e31a33972c40fda3adbecb825b4d
overdrive1708 <52090022+overdrive1708@users.noreply.github.com>
2024-08-03 16:43:30 +0900

    Add new application.

---
+++ src/Models/Configuration.cs
@@ -0,0 +1,73 @@
+﻿using System.IO;
+using System.Text.Json;
+
+namespace CryptoUtilities.Models
+{
+    /// <summary>
+    /// 設定クラス
+    /// </summary>
+    public class Configuration
+    {
+        /// <summary>
+        /// AES:CBCモードかどうか
+        /// </summary>
+        public bool IsAesModeCBC {  get; set; }
+
+        /// <summary>
+        /// AES:ECBモードかどうか
+        /// </summary>
+        public bool IsAesModeECB { get; set; }
+
+        /// <summary>
+        /// AES:キー
+        /// </summary>
+        public string AesKey {  get; set; }
+
+        /// <summary>
+        /// AES:初期化ベクター
+        /// </summary>
+        public string AesInitializationVector { get; set; }
+
+        /// <summary>
+        /// 設定ファイル名
+        /// </summary>
+        private string _configFileName = @"Configuration.json";
+
+        /// <summary>
+        /// 設定保存処理
+        /// </summary>
+        public void Save()
+        {
+            using FileStream fs = File.Create(_configFileName);
+            using StreamWriter sw = new(fs, System.Text.Encoding.UTF8);
+            JsonSerializerOptions options = new() { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(this, options);
+            sw.WriteLine(jsonString);
+        }
+
+        /// <summary>
+        /// 設定読み出し処理
+        /// </summary>
+        public void Load()
+        {
+            if (File.Exists(_configFileName))
+            {
+                using FileStream fs = File.OpenRead(_configFileName);
+                using StreamReader sr = new(fs, System.Text.Encoding.UTF8);
+                Configuration readConfig = JsonSerializer.Deserialize<Configuration>(sr.ReadToEnd());
+                IsAesModeCBC = readConfig.IsAesModeCBC;
+                IsAesModeECB = readConfig.IsAesModeECB;
+                AesKey = readConfig.AesKey;
+                AesInitializationVector = readConfig.AesInitializationVector;
+            }
+            else
+            {
+                // ファイルがないときはデフォルト値で動作する
+                IsAesModeCBC = false;
+                IsAesModeECB = true;
+                AesKey = @"00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00";
+                AesInitializationVector = @"00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00";
+            }
+        }
+    }
+}
---
+++ src/Models/ExceptionHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace CryptoUtilities.Models
+{
+    /// <summary>
+    /// 例外ハンドラークラス
+    /// </summary>
+    public class ExceptionHandler
+    {
+        //--------------------------------------------------
+        // メソッド
+        //--------------------------------------------------
+        /// <summary>
+        /// DispatcherUnhandledExceptionイベント発生時の処理
+        /// </summary>
+        /// <param name="sender">イベントソース</param>
+        /// <param name="e">イベントデータ</param>
+        public static void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e) => HandleException(e.Exception);
+
+        /// <summary>
+        /// UnobservedTaskExceptionイベント発生時の処理
+        /// </summary>
+        /// <param name="sender">イベントソース</param>
+        /// <param name="e">イベントデータ</param>
+        public static void OnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e) => HandleException(e.Exception.InnerException);
+
+        /// <summary>
+        /// UnhandledExceptionイベント発生時の処理
+        /// </summary>
+        /// <param name="sender">イベントソース</param>
+        /// <param name="e">イベントデータ</param>
+        public static void OnUnhandledException(object sender, UnhandledExceptionEventArgs e) => HandleException((Exception)e.ExceptionObject);
+
+        /// <summary>
+        /// 例外発生時の処理
+        /// </summary>
+        /// <param name="e">例外情報</param>
+        private static void HandleException(Exception exception)
+        {
+            _ = MessageBox.Show(exception.ToString(), Resources.Strings.Error, MessageBoxButton.OK, MessageBoxImage.Error);
+
+            // 終了する｡
+            Environment.Exit(1);
+        }
+    }
+}
